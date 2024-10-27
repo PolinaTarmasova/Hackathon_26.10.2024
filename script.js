93db0c81b9e9e933aa27eabdee8f26179158a50f
PolinaTarmasova <polina.tarmasova@gmail.com>
2024-10-28 00:50:37 +0500

    test0

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let elements = document.querySelectorAll(".element")
 let hasAnimated = false;
 
 document.addEventListener("scroll", () => {
-    if (window.scrollY > 100 && !hasAnimated) {
+    if (window.scrollY > 70 && !hasAnimated) {
         hasAnimated = true;
         partTop.style.transform = "translate3d(0, 0, 0) rotateX(0deg)";
         mockup.classList.add("loaded");
         elements.forEach(function (element) {
             element.classList.remove("hidden-oval")
-            element.classList.add("vivible-oval")
+            element.classList.add("visible-oval")
         });
     }
 })
